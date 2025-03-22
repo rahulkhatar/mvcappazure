a2f8b613a13fddbcd77bf9d7d596b66fb8b3906d
rahulkhatar <rahul.k.mc@prypto.com>
2025-03-22 19:56:13 +0530

    Add project files.

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public ProductController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            ViewData["Message"] = _configuration["Message"];
+            return View();
+        }
+    }
+}
